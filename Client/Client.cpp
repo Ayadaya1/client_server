5de2c43c3cc29a455e3999f399882fba2cdccda9
Ayadaya <i509b22@voenmeh.ru>
2023-05-28 18:38:37 +0300

    kinda done

---
+++ Client/Client.cpp
@@ -0,0 +1,300 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <WinSock2.h>
+#include <string.h>
+#include <iostream>
+#include "../Packet/Packet.h"
+#include <locale.h>
+#include "../Packet/Note.h"
+#include <sstream>
+#include <ios>
+#include <cstring>
+
+int main() 
+{
+    SetConsoleCP(1251);
+    char* locale = setlocale(LC_ALL, "Russian");
+    // Инициализируем DLL
+    WSADATA wsaData;
+    WSAStartup(MAKEWORD(2, 2), &wsaData);
+    // Создаем сокет
+    // Инициируем запрос к серверу
+    const char* menuString = "1.Добавить заметку\n2.Просмотр заметок\n3.Удалить заметку\n4.Редактировать заметку\n5.Логи\n0.Выход\n\nВыберите пункт из меню: ";
+    int nSize;
+    // Получение данных, возвращаемых сервером
+    char menu;
+    bool running = true;
+    char data[252] = {0};
+    while (running)
+    {
+        char szBuffer[260] = { 0 };
+        SOCKET sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+        sockaddr_in sockAddr;
+        memset(&sockAddr, 0, sizeof(sockAddr)); // Каждый байт заполняется 0
+        sockAddr.sin_family = PF_INET;
+        sockAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+        sockAddr.sin_port = htons(1234);
+        connect(sock, (SOCKADDR*)&sockAddr, sizeof(SOCKADDR));
+        system("cls");
+        std::cout << menuString;
+        std::cin >> menu;
+        switch (menu)
+        {
+        case'1':
+        {            
+            char fc = 65;
+            int y;
+            int m;
+            int d;
+            std::cout << "Введите год: ";
+            std::cin >> y;
+            std::cout << "Введите месяц(1-12): ";
+            std::cin >> m;
+            std::cout << "Введите день: ";
+            std::cin >> d;
+            std::cout << "Введите заметку: ";
+            char message[240];
+            std::cin.ignore();
+            std::cin.getline(message,240);
+
+            std::cout << message;
+            system("pause");
+            Note note(y,m,d, message);
+            char ymd[252] = { 0 };
+            system("cls");
+            _itoa(note.date.year, ymd, 10);
+            strcpy(data, ymd);
+            strcat(data, " ");
+            _itoa(note.date.month, ymd, 10);
+            strcat(data, ymd);
+            strcat(data, " ");
+            _itoa(note.date.day, ymd, 10);
+            strcat(data, ymd);
+            strcat(data, " ");
+            strcat(data, message);
+            Packet pack(fc, data);
+            send(sock, pack.Serialize(), 260, NULL);
+            recv(sock, szBuffer, 260, NULL);
+            pack.Deserialize(szBuffer);
+            std::cout << pack.Data << std::endl;
+            system("pause");
+            break;
+        }
+        case '2':
+        {
+            char fc = 66;
+            
+            Packet pack(fc, data);
+            send(sock, pack.Serialize(), 260, NULL);
+            system("cls");
+            std::cout << "Отправлен пакет: " << std::endl << std::endl;
+            pack.Print();
+            system("pause");
+            system("cls");
+            recv(sock, szBuffer, 260, NULL);
+            pack.Deserialize(szBuffer);
+            int n;
+            n = atoi(pack.Data);
+
+            if (n == 0)
+            {
+                std::cout << "Заметок не найдено!" << std::endl;
+            }
+            else {
+                for (int i = 0; i < n; i++)
+                {
+
+                    recv(sock, szBuffer, 260, NULL);
+                    pack.Deserialize(szBuffer);
+                    std::stringstream s;
+                    s << pack.Data;
+                    Note note(s);
+                    std::cout << i + 1 << ")" << (int)note.date.day << "." << (int)note.date.month << "." << (int)note.date.year << ": " << note.note << std::endl;
+
+                }
+            }
+            system("pause");
+            break;
+        }
+        case '3':
+        {
+            char fc = 67;
+            int y;
+            int m;
+            int d;
+            std::cout << "Введите год: ";
+            std::cin >> y;
+            std::cout << "Введите месяц(1-12): ";
+            std::cin >> m;
+            std::cout << "Введите день: ";
+            std::cin >> d;
+            char ymd[252] = { 0 };
+            _itoa(y, ymd, 10);
+            strcpy(data, ymd);
+            strcat(data, " ");
+            _itoa(m, ymd, 10);
+            strcat(data, ymd);
+            strcat(data, " ");
+            _itoa(d, ymd, 10);
+            strcat(data, ymd);
+            Packet pack(fc, data);
+            send(sock, pack.Serialize(), 260, NULL);
+            system("cls");
+            std::cout << "Отправлен пакет: " << std::endl << std::endl;
+            pack.Print();
+            system("pause");
+            system("cls");
+            recv(sock, szBuffer, 260, NULL);
+            pack.Deserialize(szBuffer);
+            int n;
+            n = atoi(pack.Data);
+
+            if (n == 0)
+            {
+                std::cout << "Заметок не найдено!" << std::endl;
+            }
+            else {
+                for (int i = 0; i < n; i++)
+                {
+
+                    recv(sock, szBuffer, 260, NULL);
+                    pack.Deserialize(szBuffer);
+                    std::stringstream s;
+                    s << pack.Data;
+                    Note note(s);
+                    std::cout << i + 1 << ")" << (int)note.date.day << "." << (int)note.date.month << "." << (int)note.date.year << ": " << note.note << std::endl;
+                }
+
+                std::cout << "Выберите запись для удаления или нажмите 0, чтобы ничего не удалять: ";
+                int choice;
+                std::cin >> choice;
+                char ch[5];
+                _itoa(choice, ch, 10);
+                strcpy(pack.Data, ch);
+                send(sock, pack.Serialize(), 260, NULL);
+                recv(sock, szBuffer, 260, NULL);
+                pack.Deserialize(szBuffer);
+                std::cout << pack.Data << std::endl;
+            }
+            system("pause");
+            break;
+        }
+        case '4':
+        {
+            char fc = 68;
+            int y;
+            int m;
+            int d;
+            std::cout << "Введите год: ";
+            std::cin >> y;
+            std::cout << "Введите месяц(1-12): ";
+            std::cin >> m;
+            std::cout << "Введите день: ";
+            std::cin >> d;
+            char ymd[252] = { 0 };
+            _itoa(y, ymd, 10);
+            strcpy(data, ymd);
+            strcat(data, " ");
+            _itoa(m, ymd, 10);
+            strcat(data, ymd);
+            strcat(data, " ");
+            _itoa(d, ymd, 10);
+            strcat(data, ymd);
+            Packet pack(fc, data);
+            send(sock, pack.Serialize(), 260, NULL);
+            system("cls");
+            std::cout << "Отправлен пакет: " << std::endl << std::endl;
+            pack.Print();
+            system("pause");
+            system("cls");
+            recv(sock, szBuffer, 260, NULL);
+            pack.Deserialize(szBuffer);
+            int n;
+            n = atoi(pack.Data);;
+
+            if (n == 0)
+            {
+                std::cout << "Заметок не найдено!" << std::endl;
+            }
+            else {
+                for (int i = 0; i < n; i++)
+                {
+
+                    recv(sock, szBuffer, 260, NULL);
+                    pack.Deserialize(szBuffer);
+                    std::stringstream s;
+                    s << pack.Data;
+                    Note note(s);
+                    std::cout << i + 1 << ")" << (int)note.date.day << "." << (int)note.date.month << "." << (int)note.date.year << ": " << note.note << std::endl;
+                }
+                std::cout << "Выберите запись для редактирования или нажмите 0, чтобы ничего не удалять: ";
+                int choice;
+                std::cin >> choice;
+                char ch[5];
+                _itoa(choice, ch, 10);
+                strcpy(pack.Data, ch);
+                send(sock, pack.Serialize(), 260, NULL);
+                recv(sock, szBuffer, 260, NULL);
+                pack.Deserialize(szBuffer);
+                if (strcmp(pack.Data, "Успешно") == 0)
+                {
+                    std::cout << "Введите новую заметку: ";
+                    char message[240];
+                    std::cin.ignore();
+                    std::cin.getline(message, 240);
+                    strcpy(pack.Data, message);
+                    send(sock, pack.Serialize(), 260, NULL);
+                }
+                else
+                {
+                    std::cout << pack.Data << std::endl;
+                }
+            }
+            system("pause");
+            break;
+        }
+        case '5':
+        {
+            char fc = 69;
+
+            Packet pack(fc, data);
+            send(sock, pack.Serialize(), 260, NULL);
+            system("cls");
+            std::cout << "Отправлен пакет: " << std::endl << std::endl;
+            pack.Print();
+            system("pause");
+            system("cls");
+            recv(sock, szBuffer, 260, NULL);
+            pack.Deserialize(szBuffer);
+            int n;
+            n = atoi(pack.Data);
+
+
+            for (int i = 0; i < n; i++)
+            {
+
+                recv(sock, szBuffer, 260, NULL);
+                pack.Deserialize(szBuffer);
+                std::stringstream s;
+                std::cout << i + 1 << ")" << pack.Data << std::endl;
+
+            }
+            system("pause");
+            break;
+        }
+        case'0':
+        {
+            running = false;
+            closesocket(sock);
+            break;
+        }
+        }
+    }
+    // Закрываем сокет
+    // Прекращаем использование DLL
+    WSACleanup();
+    system("pause");
+    return 0;
+}
---
+++ Server/Server.cpp
@@ -0,0 +1,340 @@
+﻿#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <winsock2.h>
+#include <iostream>
+#include "../Packet/Packet.h"
+#include <locale.h>
+#include <string.h>
+#include <cstring>
+#include <sstream>
+#include "../Packet/Note.h"
+#include <ios>
+#include <fstream>
+#include <vector>
+#pragma comment (lib, "ws2_32.lib") 
+
+int main() {
+    char* locale = setlocale(LC_ALL, "Russian");
+    WSADATA wsaData;
+    WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+    SOCKET servSock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+    sockaddr_in sockAddr;
+    memset(&sockAddr, 0, sizeof(sockAddr));
+
+    sockAddr.sin_family = PF_INET;
+    sockAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    sockAddr.sin_port = htons(1234);
+    bind(servSock, (SOCKADDR*)&sockAddr, sizeof(SOCKADDR));
+
+    listen(servSock, 20);
+    const char* filename = "notes.bin";
+    const char* filename1 = "logs.bin";
+    std::ifstream input(filename, std::ios_base::in | std::ios_base::binary);
+    std::ifstream input1(filename1, std::ios_base::in | std::ios_base::binary);
+    Note temp;
+    std::vector<Note> notes;
+    std::vector<Log> logs;
+    while (input.read((char*)&temp, sizeof(Note)))
+    {
+        notes.push_back(temp);
+    }
+    Log tempLog;
+    while (input1.read((char*)&tempLog, sizeof(Log)))
+    {
+        logs.push_back(tempLog);
+    }
+    input.close();
+    input1.close();
+    SOCKADDR clntAddr;
+    int nSize;
+    SOCKET clntSock;
+    bool running = true;
+    while (running)
+    { 
+        nSize = sizeof(SOCKADDR);
+        clntSock = accept(servSock, (SOCKADDR*)&clntAddr, &nSize);
+        char szBuffer[260] = { 0 };
+        recv(clntSock, szBuffer, 260, NULL);
+        Packet pack;
+        std::cout << std::endl;
+        pack.Deserialize(szBuffer);
+        std::cout << "Получен пакет: " << std::endl << std::endl;
+        
+        pack.Print();
+
+        switch (pack.FC)
+        {
+        case 65:
+        {
+            char str[30] = "Добавление";
+            logs.push_back(Log(str));
+            std::ofstream ofs1(filename1, std::ios::binary | std::ios::out);
+            for (int i = 0; i < logs.size(); i++)
+            {
+                logs[i].write(ofs1);
+            }
+            char buffer[252] = { 0 };
+            //strcpy(pack.Data, _itoa(n, buffer, 10));
+            //send(clntSock, pack.Serialize(), 260, NULL);
+            std::stringstream s;
+            char word[252] = {0};
+            s << pack.Data;
+            
+            Note note(s);
+            std::cout << note.date.day<<"."<<note.date.month <<"."<< note.date.year << ": "<<note.note;
+            std::ofstream ofs(filename, std::ios::binary | std::ios::out);
+            notes.push_back(note);
+            for (int i = 0; i < notes.size(); i++)
+            {
+                notes[i].write(ofs);
+            }
+            const char* str1 = "Успешно добавлена запись!";
+            strcpy(pack.Data, str1);
+            send(clntSock, pack.Serialize(), 260, NULL);
+            break;
+        }
+        case 66:
+        {
+            char str[30] = "Просмотр";
+            logs.push_back(Log(str));
+            std::ofstream ofs1(filename1, std::ios::binary | std::ios::out);
+            for (int i = 0; i < logs.size(); i++)
+            {
+                logs[i].write(ofs1);
+            }
+            int n = notes.size();
+            char nChar[10];
+            _itoa(n, nChar, 10);
+            strcpy(pack.Data, nChar);
+            send(clntSock, pack.Serialize(), 260, NULL);
+            for (int i = 0; i < n; i++)
+            {
+                char ymd[252] = { 0 };
+                char data[252] = { 0 };
+                _itoa(notes[i].date.year, ymd, 10);
+                strcpy(data, ymd);
+                strcat(data, " ");
+                _itoa(notes[i].date.month, ymd, 10);
+                strcat(data, ymd);
+                strcat(data, " ");
+                _itoa(notes[i].date.day, ymd, 10);
+                strcat(data, ymd);
+                strcat(data, " ");
+                strcat(data, notes[i].note);
+                strcpy(pack.Data, data);
+                std::cout << pack.Data << std::endl;
+                send(clntSock, pack.Serialize(), 260, NULL);
+            }
+            break;
+        }
+        case 67:
+        {
+            char str[30] = "Удаление";
+            logs.push_back(Log(str));
+            std::ofstream ofs1(filename1, std::ios::binary | std::ios::out);
+            for (int i = 0; i < logs.size(); i++)
+            {
+                logs[i].write(ofs1);
+            }
+            std::stringstream s;
+            s << pack.Data;
+            char temp[10];
+            s >> temp;
+            int y = atoi(temp);
+            s >> temp;
+            int m = atoi(temp);
+            s >> temp;
+            int d = atoi(temp);
+            std::vector<Note> found;
+            for (int i = 0; i < notes.size(); i++)
+            {
+                if (notes[i].date.year == y && notes[i].date.month == m && notes[i].date.day == d)
+                {
+                    found.push_back(notes[i]);
+                }
+            }
+            int n = found.size();
+            char nChar[10];
+            _itoa(n, nChar, 10);
+            strcpy(pack.Data, nChar);
+            send(clntSock, pack.Serialize(), 260, NULL);
+            for (int i = 0; i < n; i++)
+            {
+                char ymd[252] = { 0 };
+                char data[252] = { 0 };
+                _itoa(found[i].date.year, ymd, 10);
+                strcpy(data, ymd);
+                strcat(data, " ");
+                _itoa(found[i].date.month, ymd, 10);
+                strcat(data, ymd);
+                strcat(data, " ");
+                _itoa(found[i].date.day, ymd, 10);
+                strcat(data, ymd);
+                strcat(data, " ");
+                strcat(data, found[i].note);
+                strcpy(pack.Data, data);
+                std::cout << pack.Data << std::endl;
+                send(clntSock, pack.Serialize(), 260, NULL);
+            }if (n != 0)
+            {
+                recv(clntSock, szBuffer, 260, NULL);
+                pack.Deserialize(szBuffer);
+                int num = atoi(pack.Data);
+                char message[30];
+                if (num == 0)
+                {
+                    strcpy(message, "Отменено успешно!");
+                }
+                else if (num > n)
+                {
+                    strcpy(message, "Ошибка!");
+                }
+                else
+                {
+                    strcpy(message, "Удаление успешно!");
+                    auto it = std::remove(notes.begin(), notes.end(), found.at(num - 1));
+                    std::cout << (*it).note;
+                    notes.erase(std::remove(notes.begin(), notes.end(), found.at(num - 1)), notes.end());
+                    std::ofstream ofs(filename, std::ios::binary | std::ios::out);
+                    for (int i = 0; i < notes.size(); i++)
+                    {
+                        notes[i].write(ofs);
+                    }
+                }
+                strcpy(pack.Data, message);
+                send(clntSock, pack.Serialize(), 260, NULL);
+            }
+            break;
+        }
+        case 68:
+        {
+            char str[30] = "Редактирование";
+            logs.push_back(Log(str));
+            std::ofstream ofs1(filename1, std::ios::binary | std::ios::out);
+            for (int i = 0; i < logs.size(); i++)
+            {
+                logs[i].write(ofs1);
+            }
+            std::stringstream s;
+            s << pack.Data;
+            char temp[10];
+            s >> temp;
+            int y = atoi(temp);
+            s >> temp;
+            int m = atoi(temp);
+            s >> temp;
+            int d = atoi(temp);
+            std::vector<Note> found;
+            for (int i = 0; i < notes.size(); i++)
+            {
+                if (notes[i].date.year == y && notes[i].date.month == m && notes[i].date.day == d)
+                {
+                    found.push_back(notes[i]);
+                }
+            }
+            int n = found.size();
+            char nChar[10];
+            _itoa(n, nChar, 10);
+            strcpy(pack.Data, nChar);
+            send(clntSock, pack.Serialize(), 260, NULL);
+            for (int i = 0; i < n; i++)
+            {
+                char ymd[252] = { 0 };
+                char data[252] = { 0 };
+                _itoa(found[i].date.year, ymd, 10);
+                strcpy(data, ymd);
+                strcat(data, " ");
+                _itoa(found[i].date.month, ymd, 10);
+                strcat(data, ymd);
+                strcat(data, " ");
+                _itoa(found[i].date.day, ymd, 10);
+                strcat(data, ymd);
+                strcat(data, " ");
+                strcat(data, found[i].note);
+                strcpy(pack.Data, data);
+                std::cout << pack.Data << std::endl;
+                send(clntSock, pack.Serialize(), 260, NULL);
+            }
+            if (n != 0)
+            {
+                recv(clntSock, szBuffer, 260, NULL);
+                pack.Deserialize(szBuffer);
+                int num = atoi(pack.Data);
+                std::cout << num << std::endl;
+                char message[30];
+                if (num == 0)
+                {
+                    strcpy(message, "Отменено успешно!");
+                }
+                else if (num > n)
+                {
+                    strcpy(message, "Ошибка!");
+                }
+                else
+                {
+                    strcpy(message, "Успешно");
+                }
+                strcpy(pack.Data, message);
+                send(clntSock, pack.Serialize(), 260, NULL);
+                recv(clntSock, szBuffer, 260, NULL);
+                pack.Deserialize(szBuffer);
+                auto it = std::find(notes.begin(), notes.end(), found.at(num - 1));
+                if (it != notes.end())
+                {
+                    strcpy(it->note, pack.Data);
+                }
+                std::ofstream ofs(filename, std::ios::binary | std::ios::out);
+                for (int i = 0; i < notes.size(); i++)
+                {
+                    notes[i].write(ofs);
+                }
+            }
+            break;
+        }
+
+        case 69:
+        {
+            char str[30] = "Просмотр обращений";
+            logs.push_back(Log(str));
+            std::ofstream ofs1(filename1, std::ios::binary | std::ios::out);
+            for (int i = 0; i < logs.size(); i++)
+            {
+                logs[i].write(ofs1);
+            }
+            int n = logs.size();
+            char nChar[10];
+            _itoa(n, nChar, 10);
+            strcpy(pack.Data, nChar);
+            send(clntSock, pack.Serialize(), 260, NULL);
+            for (int i = 0; i < n; i++)
+            {
+                char ymd[252] = { 0 };
+                char data[252] = { 0 };
+                strcpy(data, logs[i].getTime());
+                strcat(data, ": ");
+                strcat(data, logs[i].call);
+                strcpy(pack.Data, data);
+                send(clntSock, pack.Serialize(), 260, NULL);
+            }
+            break;
+        }
+        default:
+        {
+            const char* str = "Неверный код функции!\n";
+            send(clntSock, str, strlen(str) + sizeof(char), NULL);
+            break;
+        }
+        }
+    }
+
+        closesocket(clntSock);
+        closesocket(servSock);
+    WSACleanup();
+    
+
+    return 0;
+}
